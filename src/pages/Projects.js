159d58293f0ce867c58987e701555a7f15c32e95
NemanjaMijucic <nemanja.mijuci@gmail.com>
2022-11-24 22:32:58 +0100

    added images

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,18 @@
 import React from "react";
+import ProjectItem from "../components/ProjectItem";
+import "../styles/projects.css";
+import Proj1 from "../assets/memorygame.png";
+import Proj2 from "../assets/weather-img.png";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <h1>My personal projects</h1>
+      <div className="projectsList">
+        <ProjectItem name="Memory game" image={Proj1} />
+        <ProjectItem name="Weather forecast" image={Proj2} />
+      </div>
+    </div>
+  );
 };
 export default Projects;
